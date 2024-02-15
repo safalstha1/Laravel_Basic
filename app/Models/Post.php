b58dd7c756815c79aeb684c21a832fbf6cea7184
safalstha1 <safal.shrestha@globalyhub.com>
2024-02-15 15:20:16 +0545

    cashing Refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,15 +28,19 @@ class Post
 
     public static function all()
         {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn($file)=> YamlFrontMatter::parseFile($file))
-                ->map(fn($document)=> new Post(
+            return cache()->rememberForever('posts.all', function (){
+                return collect(File::files(resource_path("posts")))
+                    ->map(fn($file)=> YamlFrontMatter::parseFile($file))
+                    ->map(fn($document)=> new Post(
                         $document->title,
                         $document->excerpt,
                         $document->date,
                         $document->body(),
                         $document->slug,
-                ))->sortByDesc('date');
+                    ))->sortByDesc('date');
+
+
+            });
 
         }
         public  static function find($slug)
