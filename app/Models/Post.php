b57851d40bf53636d61f4f6f9eeca1f5fa8f6d13
safalstha1 <safal.shrestha@globalyhub.com>
2024-02-22 16:40:05 +0545

    Search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,15 +2,32 @@
 
 namespace App\Models;
 
+use App\Jobs\SendPostNotificationToAdmin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use  \Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Post extends Model
 {
+
+    protected static function booted()
+    {
+        static::created(function ($post) {
+            // Dispatch the job when a new post is created
+            dispatch(new SendPostNotificationToAdmin($post));
+        });
+    }
     use HasFactory;
 
     protected $guarded = [];
     protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+        $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+    }
 
     public function category(): BelongsTo
     {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
@@ -18,22 +19,25 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
+//Route::get('/', function () {
+//
+//    return view('posts',[
+//        'posts'=> Post::latest()->get(),
+//        'categories' => Category::all()
+//    ]);
+//
+//});
+//
+//Route::get('posts/{post:slug}', function (Post $post){
+//    return view('post',[
+//       'post'=> $post
+//    ]);
+//
+//
+//});
 
-    return view('posts',[
-        'posts'=> Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-
-});
-
-Route::get('posts/{post:slug}', function (Post $post){
-    return view('post',[
-       'post'=> $post
-    ]);
-
-
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
