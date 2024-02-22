72b949ded65ecbf96172a3316a15e29dd84c269d
safalstha1 <safal.shrestha@globalyhub.com>
2024-02-22 17:05:43 +0545

    Advanced Eloquent Query Constraints

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
-    @include('_posts-header')
+    @include('posts._header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
-      <x-posts-grid :posts="$posts"/>
+            <x-posts-grid :posts="$posts"/>
         @else
             <p class="text-center"> No Posts Yet, Come Back Later</p>
         @endif
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,8 +13,10 @@
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
-                            <h5 class="font-bold">Author Name</h5>
-                            <h6>{{$post->author->name}}</h6>
+                            <h5 >Author Name</h5>
+                            <h5 class="font-bold">
+                                <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
+                            </h5>
                         </div>
                     </div>
                 </div>
